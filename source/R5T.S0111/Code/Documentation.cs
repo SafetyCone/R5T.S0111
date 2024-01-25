8f64ed9ff32876a7b61b0bfa3eb2bce4da6e928d
davidcoats <david@getsafetycone.com>
2024-01-25 12:13:28 -0800

    Initial commit.

---
+++ source/R5T.S0111/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0111
+{
+	/// <summary>
+	/// Assert (MSTest.TestFramework) scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0111/Code/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace R5T.S0111
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
